898c82476e0e8d445165b852421a892aae2bda19
Joseph Perez <hello@josephdanielperez.com>
2020-06-26 04:33:27 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,7 +7,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  words = text.scan(/\b(un)\w*(ing)/i)
+  words = text.scan(/(\b(un)\w*(ing))/i)
   return words
 end
 
