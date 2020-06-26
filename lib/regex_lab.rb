3370510229e6c14c02a5731208c043698a16c715
Joseph Perez <hello@josephdanielperez.com>
2020-06-26 04:17:58 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,13 @@
 def starts_with_a_vowel?(word)
-
+  if word.match(/\b[aeiou]\w*/i)
+    return true
+  else
+    return false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  return text.match(/\b(un)\w*(ing)/i)[0]
 end
 
 def words_five_letters_long(text)
