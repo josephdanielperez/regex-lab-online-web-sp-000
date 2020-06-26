71fc9701e86247ee64eda911ecb5c7a62de078e0
Joseph Perez <hello@josephdanielperez.com>
2020-06-26 04:18:14 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,7 +7,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  return text.match(/\b(un)\w*(ing)/i)[0]
+  return text.scan(/\b(un)\w*(ing)/i)
 end
 
 def words_five_letters_long(text)
